ae3bad95c34240ac73b29409c01d837ae6ea52a7
Krzysztof Skarbowski <krzysztof.skarbowski@gmail.com>
2026-02-20 21:23:07 +0100

    init empty spring boot project

---
+++ src/main/java/pl/xsware/XswareClientPortalApplication.java
@@ -0,0 +1,13 @@
+package pl.xsware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XswareClientPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XswareClientPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/xsware/XswareClientPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.xsware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XswareClientPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
