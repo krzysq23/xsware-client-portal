4a5a670af0783d5a72c0d2639e786bc32a7ea3d9
Krzysztof Skarbowski <krzysztof.skarbowski@gmail.com>
2026-02-21 11:33:31 +0100

    feat: add repositories for user and token

---
+++ src/main/java/pl/xsware/infrastructure/persistence/auth/RefreshTokenRepository.java
@@ -0,0 +1,10 @@
+package pl.xsware.infrastructure.persistence.auth;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+    long deleteByRevokedTrue();
+}
